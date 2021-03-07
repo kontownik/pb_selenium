5e20328731ee47a064096be27b9e0d6ef84147ad
Pawel Kamienski <x_KamiePa1@corp.onespan.com>
2021-03-07 11:26:09 +0100

    Phase 1

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,17 @@
+package runner;
+
+
+public class TestRunner {
+
+    void beforeSuite() {
+        // create driver
+    }
+
+    void afterSuite() {
+        // quit driver
+    }
+
+    void exampleTest() {
+        // exercise
+    }
+}
